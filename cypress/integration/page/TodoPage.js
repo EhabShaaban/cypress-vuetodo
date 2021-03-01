3880e9703a8549d2da5ce063be27d324d92edddc
Ehab Shaaban <ehab.shaaban@kites-software.com>
2021-03-01 21:58:13 +0200

    Refactor locators

--- cypress/integration/page/TodoPage.js
+++ cypress/integration/page/TodoPage.js
@@ -1,13 +1,27 @@
 /// <reference types="Cypress" />
 
 class TodoPage {
+
+    todoBtnByClassName(){
+        //lst of all todos
+        return cy.get('*[class^="btn border-0 flex-grow-1 text-left shadow-none"]')
+    }
+    editBtnByClassName(){
+        //lst of all edit btns
+        return cy.get('*[class^="fa fa-edit"]')
+    }
+    todoAfterEditClickBtnByClass(){
+        //appears on edit click
+        return cy.get('*[class^="flex-grow-1"]')
+    }
+
     visit() {
         cy.visit(Cypress.config().baseUrl);
         return this
     }
 
     uncheckAll() {
-        cy.get('*[class^="btn border-0 flex-grow-1 text-left shadow-none"]').each($component => {
+        this.todoBtnByClassName().each($component => {
             cy.wrap($component)
                 .wait(50)
                 .click();
@@ -15,18 +29,17 @@ class TodoPage {
     }
 
     editAll(text) {
-        cy.get('*[class^="fa fa-edit"]').each($component => {
+        this.editBtnByClassName().each($component => {
             cy.wrap($component)
                 .wait(50)
                 .click();
-            this.uncheckAll;
             cy.wrap($component).type(" "+text);
         })
     }
 
     editFirstTodoThenEnter(text){
-        cy.get('*[class^="fa fa-edit"]').eq(0).click()
-        cy.get('*[class^="flex-grow-1"]').eq(0).type(text, '{enter}')
+        this.editBtnByClassName().eq(0).click()
+        this.todoAfterEditClickBtnByClass().eq(0).type(text, '{enter}')
     }
 }
 
