7bc62ca7b6eb46ad87e84fea305647c7727c3f12
Ehab Shaaban <ehab.shaaban@kites-software.com>
2021-03-02 01:17:20 +0200

    Change test name and desc

--- cypress/integration/test/NewTodos.spec.js
+++ cypress/integration/test/NewTodos.spec.js
@@ -4,13 +4,13 @@ import TodoPage from '../page/TodoPage'
 const todoPage = new TodoPage()
 const text = require("/home/ehab/Dev/cypress-vuetodo/cypress/fixtures/foreign_text.json")
 
-describe('New todo insert tests', function () {
+describe('Add new todos tests', function () {
 
     beforeEach(() => {
         todoPage.visit();
     });
 
-    it('Should add text', function () {
+    it('Should add new todos', function () {
         for (var key in text) {
             if (text.hasOwnProperty(key)) {
                 todoPage.enterNewTodo(text[key])
